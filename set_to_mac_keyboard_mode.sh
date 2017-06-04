6f05c8384a0344b3f7fccf85c65be4ec9c3aedb7
Jie Hou <jie@pony.ai>
2017-06-04 03:16:56 -0700

    Set keyboard delay and repeat rate

--- set_to_mac_keyboard_mode.sh
+++ set_to_mac_keyboard_mode.sh
@@ -1,5 +1,6 @@
 setxkbmap -option altwin:swap_alt_win
 setxkbmap -layout us -option ctrl:nocaps
 echo 2 | gksudo tee /sys/module/hid_apple/parameters/fnmode
+xset r rate 250 30
 
 notify-send AppleKeyboardMode "Switched to Apple keyboard mode"
--- set_to_pc_keyboard_mode.sh
+++ set_to_pc_keyboard_mode.sh
@@ -1,4 +1,5 @@
 setxkbmap -option
 setxkbmap -layout us -option ctrl:nocaps
+xset r rate 250 30
 
 notify-send PCKeyboardMode "Switched to PC keyboard mode"
