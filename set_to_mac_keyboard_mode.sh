033ff05854eb0101370f925a428b56a8fb965691
Jie Hou <jie@pony.ai>
2017-06-06 11:18:34 -0700

    Change fn key config for Gnome3

--- set_to_mac_keyboard_mode.sh
+++ set_to_mac_keyboard_mode.sh
@@ -1,6 +1,7 @@
 setxkbmap -option altwin:swap_alt_win
 setxkbmap -layout us -option ctrl:nocaps
-echo 2 | gksudo tee /sys/module/hid_apple/parameters/fnmode
+#echo 2 | gksudo tee /sys/module/hid_apple/parameters/fnmode # (works in Unity)
+gksudo "sh -c 'echo \'2\' >> /sys/module/hid_apple/parameters/fnmode" # (works in Gnome)
 xset r rate 250 30
 
 notify-send AppleKeyboardMode "Switched to Apple keyboard mode"
