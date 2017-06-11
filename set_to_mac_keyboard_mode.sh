5be39071c75253942f52d736fd4d4c5f460cc69a
Jie Hou <jie@pony.ai>
2017-06-10 22:55:53 -0700

    Permenantly use fn keys on apple keyboard

--- set_to_mac_keyboard_mode.sh
+++ set_to_mac_keyboard_mode.sh
@@ -1,5 +1,5 @@
 gksudo "sh -c 'echo \'1\' >> /sys/module/hid_apple/parameters/swap_opt_cmd && sh -c 'echo \'2\' >> /sys/module/hid_apple/parameters/fnmode" # (works in Gnome)
-setxkbmap -layout us -option ctrl:nocaps
+gsettings set org.gnome.desktop.input-sources xkb-options "['ctrl:nocaps']"
 xset r rate 250 30
 
 notify-send AppleKeyboardMode "Switched to Apple keyboard mode"
--- set_to_pc_keyboard_mode.sh
+++ set_to_pc_keyboard_mode.sh
@@ -1,4 +1,4 @@
-setxkbmap -layout us -option ctrl:nocaps
+gsettings set org.gnome.desktop.input-sources xkb-options "['ctrl:nocaps']"
 xset r rate 250 30
 
 notify-send PCKeyboardMode "Switched to PC keyboard mode"
