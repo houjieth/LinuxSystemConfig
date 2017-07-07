99ade4b5882764852e58d8b5bec1c8c57bbdc68b
Jie Hou <jie@pony.ai>
2017-07-07 02:36:50 -0700

    Reduce trackpoint speed

--- set_to_pc_keyboard_mode.sh
+++ set_to_pc_keyboard_mode.sh
@@ -5,5 +5,5 @@ notify-send PCKeyboardMode "Switched to PC keyboard mode"
 
 # also mouse
 xinput set-prop "TPPS/2 IBM TrackPoint" 277 0.45
-xinput set-prop "TPPS/2 IBM TrackPoint" 279 8.5
+xinput set-prop "TPPS/2 IBM TrackPoint" 279 6.0
 
