a809866e08152a9ddebd8332207aa4e72c9ef8d9
Jie Hou <jie@pony.ai>
2017-07-27 19:56:59 -0700

    I switch back to PC!

--- set_to_pc_keyboard_mode.sh
+++ set_to_pc_keyboard_mode.sh
@@ -6,4 +6,6 @@ notify-send PCKeyboardMode "Switched to PC keyboard mode"
 # also mouse
 xinput set-prop "TPPS/2 IBM TrackPoint" 277 0.45
 xinput set-prop "TPPS/2 IBM TrackPoint" 279 6.0
-
+xinput set-prop "Logitech M705" 279 6
+xinput set-prop "ELECOM ELECOM TrackBall Mouse" 279 2.7
+xinput set-prop "Logitech M510" 279 6
