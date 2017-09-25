2dd14a0a9f638d2e2c1e4c74978e0bf323f010da
Jie Hou <jie@pony.ai>
2017-09-25 22:23:22 +0800

    Now I use both PC and Mac keyboard for work

--- set_to_mac_keyboard_mode.sh
+++ set_to_mac_keyboard_mode.sh
@@ -1,10 +1,17 @@
+# swap alt and win key setting & fn key setting
 gksudo "sh -c 'echo \'1\' >> /sys/module/hid_apple/parameters/swap_opt_cmd && sh -c 'echo \'2\' >> /sys/module/hid_apple/parameters/fnmode" # (works in Gnome)
+
+# ctrl as caps
 gsettings set org.gnome.desktop.input-sources xkb-options "['ctrl:nocaps']"
+
+# repeat rate and delay
 xset r rate 250 30
 
-notify-send AppleKeyboardMode "Switched to Apple keyboard mode"
-
 # also mouse
+xinput set-prop "TPPS/2 IBM TrackPoint" 277 0.45
+xinput set-prop "TPPS/2 IBM TrackPoint" 279 6.0
 xinput set-prop "Logitech M705" 279 6
 xinput set-prop "ELECOM ELECOM TrackBall Mouse" 279 2.7
 xinput set-prop "Logitech M510" 279 6
+
+notify-send AppleKeyboardMode "Switched to Apple keyboard mode"
--- set_to_pc_keyboard_mode.sh
+++ set_to_pc_keyboard_mode.sh
@@ -1,7 +1,11 @@
-gsettings set org.gnome.desktop.input-sources xkb-options "['ctrl:nocaps']"
-xset r rate 250 30
+# swap alt and win key setting & fn key setting
+gksudo "sh -c 'echo \'0\' >> /sys/module/hid_apple/parameters/swap_opt_cmd && sh -c 'echo \'1\' >> /sys/module/hid_apple/parameters/fnmode" # (works in Gnome)
 
-notify-send PCKeyboardMode "Switched to PC keyboard mode"
+# ctrl as caps
+gsettings set org.gnome.desktop.input-sources xkb-options "['ctrl:nocaps']"
+
+# repeat rate and delay
+xset r rate 250 30
 
 # also mouse
 xinput set-prop "TPPS/2 IBM TrackPoint" 277 0.45
@@ -9,3 +13,5 @@ xinput set-prop "TPPS/2 IBM TrackPoint" 279 6.0
 xinput set-prop "Logitech M705" 279 6
 xinput set-prop "ELECOM ELECOM TrackBall Mouse" 279 2.7
 xinput set-prop "Logitech M510" 279 6
+
+notify-send PCKeyboardMode "Switched to PC keyboard mode"
