cd4e77d81078d7566330549b9feba626b31c239b
Jie Hou <jie@pony.ai>
2018-04-06 00:08:21 -0700

    Text scaling setting

---
+++ set_to_2x_dpi.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+gsettings reset com.ubuntu.user-interface scale-factor
+gsettings reset org.gnome.desktop.interface scaling-factor
+gsettings reset com.canonical.Unity.Interface text-scale-factor
+gsettings reset org.gnome.desktop.interface text-scaling-factor
