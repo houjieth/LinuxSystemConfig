2dd64e0b3a7cd4dc3a2a566be434383e42b8bf34
Jie Hou <jie@pony.ai>
2020-01-28 16:18:33 +0800

    More tweak to trackpoint param

--- set_to_pc_keyboard_mode.sh
+++ set_to_pc_keyboard_mode.sh
@@ -6,8 +6,12 @@ setxkbmap -option ctrl:nocaps
 xset r rate 250 30
 
 # also mouse
-xinput set-prop "TPPS/2 Elan TrackPoint" 278 0.9
-xinput set-prop "TPPS/2 Elan TrackPoint" 280 6.0
+xinput set-prop "TPPS/2 Elan TrackPoint" 278 1
+xinput set-prop "TPPS/2 Elan TrackPoint" 279 4
+xinput set-prop "TPPS/2 Elan TrackPoint" 280 8
+#xinput set-prop "TPPS/2 Elan TrackPoint" 283 1
+#xinput set-prop "TPPS/2 Elan TrackPoint" 284 4
+#xinput set-prop "TPPS/2 Elan TrackPoint" 285 8
 xinput set-prop "Logitech M705" 279 6
 xinput set-prop "ELECOM ELECOM TrackBall Mouse" 279 2.7
 xinput set-prop "Logitech M510" 279 6
