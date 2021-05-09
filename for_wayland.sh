b8039da2277852f9957d7b1fcb12525b65e65529
Jie Hou <jie@pony.ai>
2021-05-09 16:53:44 +0800

    Add capslock remap for Wayland

---
+++ for_wayland.sh
@@ -0,0 +1,4 @@
+# In Gnome, use capslock as ctrl
+gsettings set org.gnome.desktop.input-sources xkb-options "['caps:ctrl_modifier']"
+
+# For key repeat delay and repeat rate, you can adjust it in Universal Access in Gnome settings
